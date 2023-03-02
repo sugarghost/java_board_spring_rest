1e2609fa3d3344b8e66984c0e98f4d24560b50ef
sugarghost <code.sugartoast@gmail.com>
2023-03-03 01:35:16 +0900

    결국 에러나서 다시 돌리고 초기 상태 저장

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,16 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
+import { createApp } from "vue";
+import axios from "axios";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-createApp(App).use(store).use(router).mount('#app');
+const app = createApp(App);
+
+app.config.globalProperties.$store = store;
+
+app.config.globalProperties.$axios = axios;
+
+app.use(router);
+app.use(store);
+
+app.mount("#app");
